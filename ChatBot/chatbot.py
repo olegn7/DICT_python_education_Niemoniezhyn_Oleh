8e10fb24545c0e7a377a7a1e8ab160ef98a37af0
Niemoniezhyn_Oleh <o.r.niemoniezhyn@student.khai.edu>
2024-12-29 12:33:49 +0200

    ChatBot 1-st stage
    ChatBot 2-nd stage
    
    ChatBot 3-rd stage
    
    Chatbot 4-th stage: count to 4
    
    ChatBot final stage updated test question

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,7 +1,21 @@
-KAN_bot = "KAN_Bot"
-birth_year = 2024
-print(f"Hello! My name is {KAN_bot}.")
-print(f"I was created in {birth_year}.")
-print("Please, remind me your name.")
-Oleh = input("> ")
-print(f"What a great name you have, {Oleh}!")
+print("Hello! My Name is KAN_bot. ")
+print("I was created in 2024. ")
+print(" Please, remind me your name. ")
+user_name = input("> Oleh")
+print(f"What a great name you have, {user_name}! ")
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+print("> 0")
+print("> 3")
+print("> 4")
+rem3 = 0
+rem5 = 3
+rem7 = 4
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+print(f"Your age is {age}; that's a good time to start programming!")
+print("Now I will prove to you that I can count to any number you want.")
+print("> 4")
+num = 4
+for i in range(num + 1):
+    print(f"{i} !")
+print("Completed, have a nice day!")
