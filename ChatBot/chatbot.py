f4340d1e43b2c6f8cc5e954c5b22f7277e37f70e
Niemoniezhyn_Oleh <o.r.niemoniezhyn@student.khai.edu>
2024-12-29 08:31:46 +0200

    ChatBot 1-st stage
    ChatBot 2-nd stage
    
    ChatBot 3-rd stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,7 @@
+KAN_bot = "KAN_Bot"
+birth_year = 2024
+print(f"Hello! My name is {KAN_bot}.")
+print(f"I was created in {birth_year}.")
+print("Please, remind me your name.")
+Oleh = input("> ")
+print(f"What a great name you have, {Oleh}!")
